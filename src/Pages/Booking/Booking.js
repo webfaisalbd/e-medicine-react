181453e0840854b8469dc4b4980a7cfe2589b2eb
unknown <web.faisal.bd@gmail.com>
2021-10-19 09:53:18 +0600

    dynamic route and show details after button click

--- src/Pages/Booking/Booking.js
+++ src/Pages/Booking/Booking.js
@@ -1,12 +1,27 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { useParams } from 'react-router';
 
 const Booking = () => {
     const { serviceId } = useParams();
+
+
+const [serviceDetails,setServiceDetails]=useState([])
+
+useEffect(()=>{
+    fetch('/services.json')
+    .then(res=>res.json())
+    .then(data=>setServiceDetails(data))
+},[])
+
+const foundDetails= serviceDetails.find(singleService=>singleService.id===Number(serviceId))
+//  console.log(foundDetails);
+
+
     return (
         <div>
-            <p>this is booking: {serviceId} </p>
-            <p>{serviceId.img}</p>
+            <p> {foundDetails?.name} </p>
+            <p> {foundDetails?.description} </p>
+            
         </div>
     );
 };
